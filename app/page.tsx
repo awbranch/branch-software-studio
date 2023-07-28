e914c5c39e73a994423fa10fc1a9adbd5248ba0e
Andrew Branch <awbranch@gmail.com>
2023-07-28 11:42:46 -0400

    More progress

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,23 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-      <h1>Branch</h1>
-      <h2>Software Studio</h2>
+      <div className="relative" style={{ width: 1024, height: 1024 }}>
+        <Image
+          src={'/branchhead.jpg'}
+          style={{ objectFit: 'cover' }}
+          fill={true}
+          alt="branchhead"
+        />
+        <img
+          src={'/logo-text.svg'}
+          width={1024}
+          style={{ position: 'absolute', bottom: 20 }}
+        />
+      </div>
       <h3>
         Using innovation, design, and technology to create products that
         positively impact people&apos;s lives.
       </h3>
-
-      <div>
-        <Image
-          src="/branchhead.jpg"
-          alt="Branch Head"
-          fill={true}
-          style={{ objectFit: 'contain' }}
-          priority
-        />
-      </div>
     </main>
   );
 }
