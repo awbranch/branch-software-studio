c916111d3a5bea8a6e1314301246335b08082e3a
Andrew Branch <awbranch@gmail.com>
2023-07-28 20:36:40 -0400

    More progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './globals.css';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const poppins = Poppins({ weight: ['400', '600', '800'], subsets: ['latin'] });
 
 export const metadata = {
   title: 'Branch Software Studio',
-  description: 'Software consulting and product development',
+  description:
+    'Product development, branding, UI/UX, web development, mobile development, SEO and marketing.',
 };
+
+// TODO: Add OpenGraph and Twitter metadata.
+// See https://nextjs.org/docs/app/api-reference/file-conventions/metadata/opengraph-image
 
 export default function RootLayout({
   children,
@@ -15,8 +19,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="scroll-smooth">
+      <body className={`${poppins.className} bg-gray-950 text-white`}>
+        <header className="fixed top-0 z-20 w-full">
+          <nav className="mx-auto max-w-5xl px-6 py-12">
+            <div className="flex items-center justify-between">
+              <a href="/" className="text-3xl font-extrabold tracking-widest">
+                BRANCH
+              </a>
+              <a
+                href="#contact"
+                className="relative py-1.5 before:absolute before:inset-0 before:origin-bottom before:scale-y-[.03] before:bg-white/60 before:transition before:duration-300 hover:before:scale-y-100 hover:before:scale-x-125 hover:before:bg-white/10"
+              >
+                <span className="relative text-lg">let&apos;s talk</span>
+              </a>
+            </div>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,38 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <div className="relative" style={{ width: 1024, height: 1024 }}>
-        <Image
-          src={'/branchhead.jpg'}
-          style={{ objectFit: 'cover' }}
-          fill={true}
-          alt="branchhead"
-        />
+    <main className="relative">
+      <section id="home" className="relative min-h-screen">
+        <div
+          aria-hidden="true"
+          className="absolute inset-0 z-[1] bg-gradient-to-b from-black/10 via-black/30 to-black"
+        ></div>
         <img
-          src={'/logo-text.svg'}
-          width={1024}
-          style={{ position: 'absolute', bottom: 20 }}
+          src="/branchhead.webp"
+          className="fixed inset-0 h-full w-full object-cover"
+          alt="branch head"
         />
-      </div>
-      <h3>
-        Using innovation, design, and technology to create products that
-        positively impact people&apos;s lives.
-      </h3>
+        <div className="relative z-10 mx-auto max-w-5xl px-6 pb-60 pt-60">
+          <div className="pb-12">
+            <h1
+              data-rellax-speed="-3"
+              data-rellax-xs-speed="0"
+              data-rellax-mobile-speed="0"
+              className="rellax max-w-lg font-semibold text-8xl md:text-9xl"
+            >
+              software studio
+            </h1>
+          </div>
+          <div>
+            <div className="ml-auto pt-12 md:w-1/2">
+              <p className="mb-20 text-xl leading-8 md:leading-10 md:text-2xl">
+                Branch Software Studio creates web applications that are simple,
+                creative, and human-focused.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
