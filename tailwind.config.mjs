5dd32e3adc71f09f72f3033c7903bf06cd414469
Andrew Branch <awbranch@gmail.com>
2024-05-15 19:56:11 -0400

    Added flowbite and the navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1024px',
+			'2xl': '1024px',
+		},
+		extend: {
+			colors: {
+				wood: {
+					100: '#f7f4ed',
+					200: '#e5e4e0',
+				},
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('flowbite/plugin')
+	],
 }
