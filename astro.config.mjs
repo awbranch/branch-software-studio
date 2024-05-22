e927584b4460ad46ee1ef81ad17e542a5977aea3
Andrew Branch <awbranch@gmail.com>
2024-05-22 15:32:43 -0400

    Cloudflare Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  integrations: [tailwind()],
 });
