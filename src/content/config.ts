405cea03ff318c17fea4db1a8fa10b3b5046a148
Andrew Branch <awbranch@gmail.com>
2024-05-28 14:25:38 -0400

    Gave the work pages better slugs

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { z, defineCollection } from "astro:content";
 const project = defineCollection({
   type: "content",
   schema: z.object({
+    order: z.number(),
     title: z.string(),
     categories: z.array(z.string()),
     time: z.string(),
